928122055c00824b7028d0e2cdf784ebc2ef7df3
= <james.a.padden@gmail.com>
2025-03-18 03:01:18 -0700

    Added comment explaining structure of weather data

--- weather_board/weather_manager.py
+++ weather_board/weather_manager.py
@@ -15,3 +15,23 @@ def get_weather():
     r = requests.get(url, params=params)
     data = json.loads(r.content)
     return data
+
+'''
+Weather keys:
+
+-current
+  -time
+  -temperature_2m
+  -wind_speed_10m
+  -wind_direction_10m
+  -apparent_temperature
+-hourly
+  -temperature_2m
+-daily
+  -precipitation_probability_max
+  -temperature_2m_max
+  -temperature_2m_min
+  -precipitation_sum
+  -sunrise
+  -sunset
+'''
