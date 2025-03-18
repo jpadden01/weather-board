8d20392bc7041c83a0f269f5b70167466ed7727e
= <james.a.padden@gmail.com>
2025-03-18 02:34:13 -0700

    Passed weather information to home.html.

--- weather_board/views.py
+++ weather_board/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from .weather_manager import get_weather
 
 views = Blueprint("views", __name__)
 
 @views.route('/')
 def home():
-    return render_template('home.html')
+    weather = get_weather()
+    if weather:
+        return render_template('home.html', weather=weather)
+    else:
+        # TODO: Create error page if api response fails
+        return '<p>Sorry, there was an error retrieving the weather information.</p>'
