1fdcf86c97b403612e71d1dc80856c3ca3008adc
= <james.a.padden@gmail.com>
2025-03-18 21:15:45 -0700

    Added error page in case API call fails.

--- weather_board/views.py
+++ weather_board/views.py
@@ -6,9 +6,9 @@ views = Blueprint("views", __name__)
 @views.route('/')
 def home():
     weather = get_weather()
+
     if weather:
         graph_weather(weather)
         return render_template('home.html', weather=weather)
     else:
-        # TODO: Create error page if api response fails
-        return '<p>Sorry, there was an error retrieving the weather information.</p>'
+        return render_template('error.html')
